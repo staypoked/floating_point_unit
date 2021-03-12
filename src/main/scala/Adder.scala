cb71d542f6e7dd83013585162abcf95521dc7df3
Stefan Ulmer <stefanulmer@gmx.at>
2021-03-12 19:03:46 +0100

    Adder with first test

--- src/main/scala/Adder.scala
+++ src/main/scala/Adder.scala
@@ -1,7 +1,4 @@
-import Chisel.is
 import chisel3._
-import chisel3.core.IO
-import chisel3.util.ImplicitConversions.booleanToBool
 import chisel3.util._
 
 
@@ -12,22 +9,29 @@ class Adder extends Module{
     val c = Output(UInt(32.W))
   })
 
-  //val a_sign := Cat(io.a(3,2), 0.U(2.W));
-  var a_sign = RegNext(io.a(0)); // 1 for neg
-  var a_exp = RegNext(io.a(7,1));
-  var a_mant = RegNext(io.a(31,8));
+  _root_.Chisel.printf("a_val: %b, b_val: %b\n", io.a, io.b)
 
-  var b_sign = RegNext(io.b(0));
-  var b_exp = RegNext(io.b(7,1));
-  var b_mant = RegNext(io.b(31,8));
+  val a_sign = RegNext(io.a(31), 0.U)
+  val a_exp = RegNext(io.a(30,24), 0.U)
+  val a_mant = RegNext(io.a(23,0), 0.U)
+
+
+  val b_sign = RegNext(io.b(31), 0.U)
+  val b_exp = RegNext(io.b(30,24), 0.U)
+  val b_mant = RegNext(io.b(23,0), 0.U)
+
+
+  _root_.Chisel.printf("a_sign: %d, a_exp: %b, a_mant: %b\n", a_sign, a_exp, a_mant)
+  _root_.Chisel.printf("b_sign: %d, b_exp: %b, b_mant: %b\n", b_sign, b_exp, b_mant)
+  //_root_.Chisel.printf("a_input: %b, b_input: %b\n", a_input, b_input)
 
   // output
-  var tmp_sign = RegInit(0.U(1.W));
-  var tmp_exp = RegInit(0.U(7.W));
-  var tmp_mant = RegInit(0.U(24.W));
+  val tmp_sign = RegNext(0.U(1.W))
+  val tmp_exp = RegNext(0.U(7.W))
+  val tmp_mant = RegNext(0.U(24.W))
 
-  var check_underflow = RegInit(false.B);
-  var check_overflow = RegInit(false.B);
+  val check_underflow = RegNext(false.B);
+  val check_overflow = RegNext(false.B);
 
   /*
   * Simple Double precision?
@@ -36,27 +40,34 @@ class Adder extends Module{
 
 
   // check alignment of exponents
+
+
   when(a_exp > b_exp){
     //shift lower exponent for difference
-    var shift_val = a_exp - b_exp;
+    val shift_val = a_exp - b_exp;
     b_exp := a_exp;
-    b_mant := b_mant >> shift_val; // exponent increase for b
+    b_mant := (b_mant >> shift_val).asUInt(); // exponent increase for b
   }.elsewhen(a_exp < b_exp) {
-    var shift_val = b_exp - a_exp;
+    val shift_val = b_exp - a_exp;
     a_exp := b_exp;
-    a_mant := a_mant >> shift_val;
+    a_mant := (a_mant >> shift_val).asUInt();
   }
+
+  _root_.Chisel.printf("a_sign: %d, a_exp: %d, a_mant: %d\n", a_sign, a_exp, a_mant)
+  _root_.Chisel.printf("b_sign: %d, b_exp: %d, b_mant: %d\n", b_sign, b_exp, b_mant)
+  _root_.Chisel.printf("tmp_sign: %d, tmp_exp: %d, tmp_mant: %d\n", tmp_sign, tmp_exp, tmp_mant)
 
 
   // afterwards the exponents has to equal
   tmp_exp := a_exp;
-  var opcode = RegInit(0.U);
+  val opcode = RegNext(0.U);
 
   when(!a_sign) {
     when(!b_sign) {
       // sign + +
       tmp_sign := 0.U;
-      tmp_mant := a_mant + b_mant;
+      tmp_mant := a_mant + b_mant
+      _root_.Chisel.printf("tmp_sign: %d, tmp_exp: %d, tmp_mant: %d\n", tmp_sign, tmp_exp, tmp_mant)
       opcode := 1.U;
 
     }.otherwise {
@@ -88,6 +99,7 @@ class Adder extends Module{
     }
   }
 
+  _root_.Chisel.printf("tmp_sign: %d, tmp_exp: %d, tmp_mant: %d\n", tmp_sign, tmp_exp, tmp_mant)
 
   // Over- and underflow
   switch(opcode){
@@ -118,6 +130,7 @@ class Adder extends Module{
 
 
   // output
+  _root_.Chisel.printf("io_c: %b\n", Cat(Cat(tmp_sign, tmp_exp), tmp_mant))
   io.c := Cat(Cat(tmp_sign, tmp_exp), tmp_mant);
 
 }
--- src/test/scala/AdderSpec.scala
+++ src/test/scala/AdderSpec.scala
@@ -1,14 +1,64 @@
-import chisel3.iotesters.PeekPokeTester
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters._
 import org.scalatest._
 
 class AdderTester(dut: Adder) extends PeekPokeTester(dut) {
-	  
+
+
+	/*
+	* NUMB GENERATOR
+	 */
+	for (repetitions <- 0 to 19){
+		//val a_val = WireDefault(0.U)
+		val a_val = "b00000001000000000000000000000001".U
+		val b_val = "b00000001000000000000000000000001".U
+
+		/*
+		var a_sign = 0.U(1.W)
+		var a_exp = 0.U(7.W)
+		var a_mant = 0.U(24.W)
+		var b_sign = 0.U(1.W)
+		var b_exp = 0.U(7.W)
+		var b_mant = 0.U(24.W)
+
+		var rand_sign = scala.util.Random.nextInt(1)
+		a_sign = rand_sign.asUInt()
+		rand_sign = scala.util.Random.nextInt(1)
+		b_sign = rand_sign.asUInt()
+
+
+		val rand_exp = scala.util.Random.nextInt(1)
+		a_exp = rand_exp.asUInt()
+		b_exp = rand_exp.asUInt()
+
+		var rand_mant = scala.util.Random.nextInt(40)
+		val a_mant = rand_mant.asUInt()
+		rand_mant = scala.util.Random.nextInt(40)
+		val b_mant = rand_mant.asUInt()
+*/
+		poke(dut.io.a, a_val) //We apply a value to the input
+		poke(dut.io.b, b_val) //We apply a value to the input
+
+		step(2)
+
+		/*
+		* CALC EXPECTED VALUE
+		 */
+
+		expect(dut.io.c, "b00000001000000000000000000000010".U)
+
+	}
+
+	println("*** Finished testing!! ***")
+
+	/*
 	for (value <- 0 until 16) {
-		poke(dut.io.a, value) //We apply a value to the input
-		poke(dut.io.b, value) //We apply a value to the input
+		poke(dut.io.a, a_val) //We apply a value to the input
+		poke(dut.io.b, b_val) //We apply a value to the input
 		//println(peek(dut.io.seg).toString(2).reverse.padTo(7,'0').reverse) //And check the value on the output.
 		//print7Segment(peek(dut.io.seg),value) //Here we print the result, as it would look on the 7-segment display.
-	}
+	}*/
 
 }
 
